196d5ea432938bac5b5f1a16748897c68a7a7763
Mohit Pandey <pandeymohit396@gmail.com>
2026-08-18 19:26:22 +0000

    feat: configure production CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,21 +1,33 @@
+import os
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from app.api.router import router
 
+
 app = FastAPI(
     title="DocGen-AI",
 )
+
+
+frontend_url = os.getenv(
+    "FRONTEND_URL",
+    "http://localhost:8080",
+)
+
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:8080",
         "http://127.0.0.1:8080",
+        frontend_url,
     ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
 app.include_router(router)
